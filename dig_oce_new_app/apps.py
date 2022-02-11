2f339659716c97572626df90e97ede007b2d6bda
Anik Chanda <raj.anik004@gmail.com>
2022-02-12 02:20:23 +0530

    Updated App with proper folder structure

---
+++ dig_oce_new_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DigOceNewAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'dig_oce_new_app'
--- digital_ocean_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DigitalOceanAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'digital_ocean_app'
